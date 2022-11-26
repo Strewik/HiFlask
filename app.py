17ffbab7b502efd233291449381ff9c91b4619ea
Strewik <52960396+Strewik@users.noreply.github.com>
2022-11-26 18:56:32 +0300

    retrive single record

--- app.py
+++ app.py
@@ -32,6 +32,44 @@ def RetrieveDataList():
     employees = EmployeeModel.query.all()
     return render_template('datalist.html',employees = employees)
 
+@app.route('/data/<int:id>/update',methods = ['GET','POST'])
+def update(id):
+    employee = EmployeeModel.query.filter_by(employee_id=id).first()
+    if request.method == 'POST':
+        if employee:
+            db.session.delete(employee)
+            db.session.commit()
+            name = request.form['name']
+            age = request.form['age']
+            position = request.form['position']
+            employee = EmployeeModel(employee_id=id, name=name, age=age, position = position)
+            db.session.add(employee)
+            db.session.commit()
+            return redirect(f'/data/{id}')
+        return f"Employee with id = {id} Does nit exist"
+
+    return render_template('update.html', employee = employee)
+
+@app.route('/data/<int:id>')
+def RetrieveEmployee(id):
+    employee = EmployeeModel.query.filter_by(employee_id=id).first()
+    if employee:
+        return render_template('data.html', employee = employee)
+    return f"Employee with id ={id} Doenst exist"
+
+
+@app.route('/data/<int:id>/delete', methods=['GET','POST'])
+def delete(id):
+    employee = EmployeeModel.query.filter_by(employee_id=id).first()
+    if request.method == 'POST':
+        if employee:
+            db.session.delete(employee)
+            db.session.commit()
+            return redirect('/data')
+        abort(404)
+
+    return render_template('delete.html')
+
 
 app.run(host='localhost', port=5000)
 
